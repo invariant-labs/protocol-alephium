0b384e09234755d859c756b32ec53eb1bf0fba69
Wojciech <cichockiwojciech97@gmail.com>
2024-02-24 18:27:20 +0100

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -5,16 +5,13 @@ set -e
 ALEPHIUM_STACK_DIR="alephium-stack"
 ALEPHIUM_CLI_PACKAGE="@alephium/cli@latest"
 
-# Install npm packages
-npm install
-
 # Start the Alephium devnet
 (
   cd $ALEPHIUM_STACK_DIR
   make start-devnet
 )
 
-# Compile and test using Alephium CLI from the project root directory
+# Compile and test using Alephium CLI
 npx $ALEPHIUM_CLI_PACKAGE compile 
 npx $ALEPHIUM_CLI_PACKAGE test
 
