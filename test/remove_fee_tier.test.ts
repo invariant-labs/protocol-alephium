13d941f8634ec89a7033c938c813ea5b03dbb7da
DevRozaDev <158298065+DevRozaDev@users.noreply.github.com>
2024-06-25 12:38:51 +0200

    add remove_fee_tier e2e tests

---
+++ test/remove_fee_tier.test.ts
@@ -0,0 +1,70 @@
+import { ONE_ALPH, web3 } from '@alephium/web3'
+import { getSigner } from '@alephium/web3-test'
+import { PrivateKeyWallet } from '@alephium/web3-wallet'
+import { deployInvariant, expectErrorCode } from '../src/utils'
+import { InvariantError, PercentageScale } from '../src/consts'
+import { feeTierExists, getFeeTiers, initFeeTier, removeFeeTier } from '../src/testUtils'
+
+web3.setCurrentNodeProvider('http://127.0.0.1:22973')
+
+let admin: PrivateKeyWallet
+
+describe('remove fee tier tests', () => {
+  beforeAll(async () => {
+    admin = await getSigner(ONE_ALPH * 1000n, 0)
+  })
+
+  test('remove_fee_tier', async () => {
+    const invariant = await deployInvariant(admin, 0n)
+
+    // 0.02%
+    const fee = 2n * 10n ** (PercentageScale - 4n)
+    const tickSpacing = 1n
+    const tickSpacing2 = 2n
+
+    await initFeeTier(invariant, admin, fee, tickSpacing)
+    await initFeeTier(invariant, admin, fee, tickSpacing2)
+    await removeFeeTier(invariant, admin, fee, tickSpacing)
+    let tierExists = feeTierExists(invariant, { fee, tickSpacing })[0]
+    expect(tierExists).toBeFalsy()
+
+    const feeTiers = await getFeeTiers(invariant)
+    expect(feeTiers[0]).toStrictEqual({ fee, tickSpacing: tickSpacing2 })
+    expect(feeTiers.length).toBe(1)
+  })
+
+  test('remove_non_existing_fee_tier', async () => {
+    const invariant = await deployInvariant(admin, 0n)
+
+    // 0.02%
+    const fee = 2n * 10n ** (PercentageScale - 4n)
+    const tickSpacing = 1n
+    const tickSpacing2 = 2n
+
+    await initFeeTier(invariant, admin, fee, tickSpacing)
+
+    let tierExists = feeTierExists(invariant, { fee, tickSpacing: tickSpacing2 })[0]
+    expect(tierExists).toBeFalsy()
+
+    expectErrorCode(
+      InvariantError.FeeTierNotFound,
+      removeFeeTier(invariant, admin, fee, tickSpacing2)
+    )
+  })
+
+  test('remove fee tier not admin', async () => {
+    const invariant = await deployInvariant(admin, 0n)
+
+    // 0.02%
+    const fee = 2n * 10n ** (PercentageScale - 4n)
+    const tickSpacing = 1n
+    const tickSpacing2 = 2n
+
+    await initFeeTier(invariant, admin, fee, tickSpacing)
+    await initFeeTier(invariant, admin, fee, tickSpacing2)
+
+    const notAdmin = await getSigner(ONE_ALPH * 1000n, 0)
+
+    expectErrorCode(InvariantError.NotAdmin, removeFeeTier(invariant, notAdmin, fee, tickSpacing))
+  })
+})
